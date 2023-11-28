4d2253eb379b07bfeaa13a3c4e70a191a9eb3138
orangejucieandoats <orangejuiceandoats@gmail.com>
2023-11-28 00:42:04 -0500

    Added minigame to main game

---
+++ DepthsOfDeceit/Assets/LogicScript.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Collections;
+using UnityEngine;
+
+public class LogicScript : MonoBehaviour
+{
+
+    private PlayerScript player;
+    private ShapeScript shape;
+    public AudioSource hit;
+    public GameObject backgroundObj;
+    public GameObject playerObj;
+    public GameObject shapeObj;
+    public GameObject wallsObj;
+    private bool loaded = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("MinigamePlayer").GetComponent<PlayerScript>();
+        shape = GameObject.FindGameObjectWithTag("MinigameShape").GetComponent<ShapeScript>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Tab) && !loaded) {
+            beginMinigame();
+        }
+    }
+
+    [ContextMenu("Decrease Life")] // add it to Unity
+    public void decreaseLife(int num) {
+        if (!player.invulnerable && player.lifePoints > 0) {
+            hit.Play();
+            player.lifePoints -= num;
+            player.StartCoroutine(player.setInvulnerable());
+            Debug.Log("Decreasing life " + player.lifePoints);
+        } else if (!player.invulnerable && player.lifePoints <= 0) {
+            hit.Play();
+            gameOver();
+        }
+    }
+
+    [ContextMenu("Game Over")] // add it to Unity
+    public void gameOver() {
+        Debug.Log("Game Over!");
+    }
+
+    public void beginMinigame() {
+        loaded = true;
+        BoxCollider2D[] s = wallsObj.GetComponentsInChildren<BoxCollider2D>();
+        foreach (BoxCollider2D x in s) {
+            x.enabled = true;
+        }
+        //backgroundObj.GetComponent<SpriteRenderer>().enabled = true;
+        GameObject p = Instantiate(playerObj);
+        p.transform.position = new Vector2(x: 0, y: -1);
+    }
+}
---
+++ DepthsOfDeceit/Assets/PlayerScript.cs
@@ -0,0 +1,128 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics.Tracing;
+using Unity.VisualScripting;
+using Unity.VisualScripting.ReorderableList.Element_Adder_Menu;
+using UnityEngine;
+using UnityEngine.Timeline;
+
+public class PlayerScript : MonoBehaviour
+{
+
+    public float moveSpeed = 5f;
+    public int lifePoints = 2;
+    public bool invulnerable = false;
+    private bool canDash = true;
+    public float invulnerableTime = 1f;
+    private bool isDashing;
+    public float dashPower = 10f;
+    public float dashTime = 0.2f;
+    public float dashCooldown = 1f;
+    private Direction[] d = {Direction.Right, Direction.None}; 
+    
+    public Sprite spriteLeft;
+    public Sprite spriteRight;
+    public Rigidbody2D rb;
+
+    public AudioSource dash;
+    Vector2 movement;
+    [SerializeField] private TrailRenderer tr;
+
+    private enum Direction {
+        None,
+        Right,
+        Left,
+        Up,
+        Down
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isDashing) {
+            return;
+        }
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+        if (movement.x < 0 && movement.y == 0) {
+            d[0] = Direction.Left;
+            d[1] = Direction.None;
+            gameObject.GetComponent<SpriteRenderer>().sprite = spriteLeft;
+        } else if (movement.x > 0 && movement.y == 0) {
+            d[0] = Direction.Right;
+            d[1] = Direction.None;
+            gameObject.GetComponent<SpriteRenderer>().sprite = spriteRight;
+        } else if (movement.y < 0 && movement.x == 0) {
+            d[0] = Direction.None;
+            d[1] = Direction.Up;
+        } else if (movement.y > 0 && movement.x == 0) {
+            d[0] = Direction.None;
+            d[1] = Direction.Down;
+        } else if (movement.y < 0 && movement.x < 0) {
+            d[0] = Direction.Left;
+            d[1] = Direction.Up;
+            gameObject.GetComponent<SpriteRenderer>().sprite = spriteLeft;
+        } else if (movement.y < 0 && movement.x > 0) {
+            d[0] = Direction.Right;
+            d[1] = Direction.Up;
+             gameObject.GetComponent<SpriteRenderer>().sprite = spriteRight;
+        } else if (movement.y > 0 && movement.x < 0) {
+            d[0] = Direction.Left;
+            d[1] = Direction.Down;
+            gameObject.GetComponent<SpriteRenderer>().sprite = spriteLeft;
+        } else if (movement.y > 0 && movement.x > 0) {
+            d[0] = Direction.Right;
+            d[1] = Direction.Down;
+            gameObject.GetComponent<SpriteRenderer>().sprite = spriteRight;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space) && canDash) {
+            StartCoroutine(Dash());
+        }
+    }
+
+    void FixedUpdate() {
+        if (isDashing) {
+            return;
+        }
+        rb.MovePosition(rb.position + moveSpeed * Time.fixedDeltaTime * movement.normalized);
+    }
+
+    private IEnumerator Dash() {
+        canDash = false;
+        isDashing = true;
+        invulnerable = true;
+        dash.Play();
+        if (d[0] == Direction.Right && d[1] == Direction.None) {
+            rb.velocity = new Vector2(dashPower, 0f);
+        } else if (d[0] == Direction.Left && d[1] == Direction.None) {
+            rb.velocity = new Vector2(-dashPower, 0f);
+        } else if (d[0] == Direction.None && d[1] == Direction.Up) {
+            rb.velocity = new Vector2(0f, -dashPower);
+        } else if (d[0] == Direction.None && d[1] == Direction.Down) {
+            rb.velocity = new Vector2(0f, dashPower);
+        } else if (d[0] == Direction.Right && d[1] == Direction.Up) {
+            rb.velocity = new Vector2(dashPower, -dashPower);
+        } else if (d[0] == Direction.Left && d[1] == Direction.Up) {
+            rb.velocity = new Vector2(-dashPower, -dashPower);
+        } else if (d[0] == Direction.Right && d[1] == Direction.Down) {
+            rb.velocity = new Vector2(dashPower, dashPower);
+        } else if (d[0] == Direction.Left && d[1] == Direction.Down) {
+            rb.velocity = new Vector2(-dashPower, dashPower);
+        }
+        tr.emitting = true;
+
+        yield return new WaitForSeconds(dashTime);
+        tr.emitting = false;
+        isDashing = false;
+        invulnerable = false;
+        yield return new WaitForSeconds(dashCooldown);
+        canDash = true;
+    }
+    public IEnumerator setInvulnerable() {
+        invulnerable = true;
+        yield return new WaitForSeconds(invulnerableTime);
+        invulnerable = false;
+    }
+}
