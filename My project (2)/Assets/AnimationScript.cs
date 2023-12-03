ecad0c52758df915df6eb1e5826795f6a0134721
orangejucieandoats <orangejuiceandoats@gmail.com>
2023-12-03 06:09:18 -0500

    added

--- My project (2)/Assets/AnimationScript.cs
+++ My project (2)/Assets/AnimationScript.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class AnimationScript : MonoBehaviour
 {
@@ -41,7 +42,18 @@ public class AnimationScript : MonoBehaviour
         return s;
     }
 
-    public void level1() {
-        Spawn(square, new Vector2(0, 55), new Color(0, 0, 255), Trail.HasTrail, 2, 3, true, Direction.Up, "Minigame_Shapes", "Minigame_Shapes_Trail");
+    public GameObject SpawnStationary(GameObject shape, Vector2 pos, Color col, Trail t, int rotSpeed, string layer="Minigame_Shapes", string trailLayer="Minigame_Shapes_Trail") {
+        GameObject s = Spawn(shape, pos, col, t, rotSpeed, 0, false, 0, layer, trailLayer);
+        return s;
+    }
+    public GameObject SpawnWiper(GameObject shape, Vector2 pos, Color col, Trail t, int rotSpeed, int moveSpeed, Direction dir, string layer="Minigame_Shapes", string trailLayer="Minigame_Shapes_Trail") {
+        GameObject s = Spawn(shape, pos, col, t, rotSpeed, moveSpeed, true, dir, layer, trailLayer);
+        return s;
+    }
+
+    public void Level1() {
+        //Spawn(hexagon, new Vector2(6, 55), new Color(0, 0, 255), Trail.HasTrail, 4, 4, true, Direction.Left, "Minigame_Shapes", "Minigame_Shapes_Trail");
+        //SpawnStationary(hexagon, new Vector2(0, 55), new Color(0, 0, 255), Trail.HasTrail, -1);
+        SpawnWiper(hexagon, new Vector2(6, 55), new Color(0, 0, 255), Trail.HasTrail, 4, 4, Direction.Left);
     }
 }
--- My project (2)/Assets/LogicScript.cs
+++ My project (2)/Assets/LogicScript.cs
@@ -86,7 +86,7 @@ public class LogicScript : MonoBehaviour
         if (loaded) {
             return;
         }
-        animationScript.level1();
+        animationScript.Level1();
         taskBarGameObj.SetActive(false);
         collidedPlayer = player;
         previousCamPos = new Vector3(player.GetComponentInChildren<Camera>().transform.position.x,  player.GetComponentInChildren<Camera>().transform.position.y,  player.GetComponentInChildren<Camera>().transform.position.z);
