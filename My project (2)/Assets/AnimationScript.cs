71ebd1f532d4026e5ef524155874e16060624220
orangejucieandoats <orangejuiceandoats@gmail.com>
2023-12-03 06:46:23 -0500

    Update AnimationScript.cs

--- My project (2)/Assets/AnimationScript.cs
+++ My project (2)/Assets/AnimationScript.cs
@@ -25,7 +25,7 @@ public class AnimationScript : MonoBehaviour
 
     public GameObject circle, square, triangle, hexagon, rhombus, capsule, roundedSquare;
 
-    public GameObject Spawn(GameObject shape, Vector2 pos, Color col, Trail t, int rotSpeed, int moveSpeed, bool isWiper, Direction dir, Vector3 scale, string layer, string trailLayer, bool isWarning) {
+    public GameObject Spawn(GameObject shape, Vector2 pos, Color col, Trail t, int rotSpeed, int moveSpeed, bool isWiper, int dur, Direction dir, Vector3 scale, string layer, string trailLayer, bool isWarning) {
         GameObject s = Instantiate(shape);
         s.transform.position = pos;
         s.GetComponent<TrailRenderer>().emitting = (t == Trail.HasTrail);
@@ -49,29 +49,35 @@ public class AnimationScript : MonoBehaviour
         } else {
             sScript.hittable = true;
         }
+        if (!isWiper) {
+            StartCoroutine(DestroyAfterDuration(s, dur));
+        }
         return s;
     }
 
     private IEnumerator SpawnWarning(ShapeScript sScript, SpriteRenderer sSprite, Color col) {
-        //Spawn(shape, pos, col, Trail.NoTrail, rotSpeed, 0, false, 0, scale, layer, trailLayer, );
         yield return new WaitForSeconds(1);
         sScript.hittable = true;
         col.a = 1;
         sSprite.color = col;
     }
+    private IEnumerator DestroyAfterDuration(GameObject s, int time) {
+        yield return new WaitForSeconds(time+1);
+        Destroy(s);
+    }
 
-    public GameObject SpawnStationary(GameObject shape, Vector2 pos, Color col, int rotSpeed, Vector3 scale, string layer = "Minigame_Shapes", string trailLayer = "Minigame_Shapes_Trail") {
-        GameObject s = Spawn(shape, pos, col, Trail.NoTrail, rotSpeed, 0, false, 0, scale, layer, trailLayer, true);
+    public GameObject SpawnStationary(GameObject shape, Vector2 pos, Color col, int rotSpeed, int dur, Vector3 scale, string layer = "Minigame_Shapes", string trailLayer = "Minigame_Shapes_Trail") {
+        GameObject s = Spawn(shape, pos, col, Trail.NoTrail, rotSpeed, 0, false, dur , 0, scale, layer, trailLayer, true);
         return s;
     }
     public GameObject SpawnWiper(GameObject shape, Vector2 pos, Color col, Trail t, int rotSpeed, int moveSpeed, Vector3 scale, Direction dir, string layer="Minigame_Shapes", string trailLayer="Minigame_Shapes_Trail") {
-        GameObject s = Spawn(shape, pos, col, t, rotSpeed, moveSpeed, true, dir, scale, layer, trailLayer, false);
+        GameObject s = Spawn(shape, pos, col, t, rotSpeed, moveSpeed, true, 0, dir, scale, layer, trailLayer, false);
         return s;
     }
 
     public void Level1() {
         //Spawn(hexagon, new Vector2(6, 55), new Color(0, 0, 255), Trail.HasTrail, 4, 4, true, Direction.Left, "Minigame_Shapes", "Minigame_Shapes_Trail", false);
-        SpawnStationary(hexagon, new Vector2(0, 55), new Color(0, 0, 255), -1, new Vector3(1, 1, 1));
+        SpawnStationary(hexagon, new Vector2(0, 55), new Color(0, 0, 255), -1, 1, new Vector3(1, 1, 1));
         //SpawnWiper(hexagon, new Vector2(6, 55), new Color(0, 0, 255), Trail.HasTrail, 4, 4, new Vector3(1, 1, 1), Direction.Left);
     }
 }
