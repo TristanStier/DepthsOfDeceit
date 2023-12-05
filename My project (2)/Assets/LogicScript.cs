2512e01b07de782e60e337c8a36e408b10b13ad9
orangejucieandoats <orangejuiceandoats@gmail.com>
2023-12-04 23:32:10 -0500

    added more

--- My project (2)/Assets/LogicScript.cs
+++ My project (2)/Assets/LogicScript.cs
@@ -132,7 +132,7 @@ public class LogicScript : MonoBehaviourPunCallbacks
     }
 
     private IEnumerator setUnLoaded() {
-        yield return new WaitForSeconds(2f);
+        yield return new WaitForSeconds(5f);
         loaded = false;
     }
 
